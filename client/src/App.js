d49cf4c6b7d774a6fcadfeb2fbbc2454cccf22de
royster <rcantu786@gmail.com>
2021-08-28 22:49:49 -0500

    linked success page to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Success from './pages/Success';
 import Nav from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
 import OrderHistory from './pages/OrderHistory';
